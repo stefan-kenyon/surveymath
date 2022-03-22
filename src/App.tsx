8b4d8c1ba845b4bfd0ac110ce0a26a51c300a2a6
Stefan Kenyon <stefan.kenyon@gmail.com>
2022-03-21 22:03:03 -0500

    Updated index page

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>surveymath</h1>
+        <p>A site designed to help people learn/practice survey mathematics</p>
       </header>
     </div>
   );
