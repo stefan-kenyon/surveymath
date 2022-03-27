3fea901025cdc00c4dd8972914fc0c5b6074d0b1
Stefan Kenyon <stefan.kenyon@gmail.com>
2022-03-26 22:16:13 -0500

    Adding Form Functionality
    Adding form functionality with react-hook-form and updated README.

---
+++ src/components/Form.tsx
@@ -0,0 +1,44 @@
+//import {useState} from 'react';
+import {useForm, SubmitHandler} from 'react-hook-form';
+
+interface IFormInputs {
+  email:string,
+  password:string
+}
+
+function Form() {
+
+  const {register, handleSubmit, watch, formState: {errors} } = useForm<IFormInputs>();
+
+  console.log('errors', errors);
+  console.log('watch variable email', watch('email'));
+  const formSubmitHandler: SubmitHandler<IFormInputs> = (data: IFormInputs) => {
+    console.log('submitted data', data);
+  }
+
+  return (
+    <div className="Form">
+
+        <header>
+          <h3>Form</h3>
+        </header>
+      
+        <main className='form-main'>
+
+          <form onSubmit={handleSubmit(formSubmitHandler)}>
+            <input defaultValue="example@leo.test.com" {...register('email')} />
+            <br/>
+            <input {...register('password', { required: true })} />
+            <br/>
+            {errors.password && <span>This field is required</span>}
+            <br/>
+            <input type='submit' />
+          </form>
+         
+        </main>
+    
+    </div>
+  );
+}
+
+export default Form;
